16c03b6053546bd51c5807ec5ff5da0d142db2a7
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2023-06-17 17:47:23 -0600

    Create bftpJena.c

---
+++ bftpJena.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_RESPONSE_LENGTH 1024
+#define SERVER_PORT 8889
+
+// Función para establecer la conexión con el servidor
+int establecerConexion(const char* serverIP) {
+    int mySocket;
+    struct sockaddr_in serverDir;
+
+    // Crear un socket
+    mySocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (mySocket == -1) {
+        perror("Error creando el socket");
+        return -1;
+    }
+
+    // Configurar la dirección del servidor
+    serverDir.sin_family = AF_INET;
+    serverDir.sin_addr.s_addr = inet_addr(serverIP);
+    serverDir.sin_port = htons(SERVER_PORT);
+
+    // Conectar al servidor
+    if (connect(mySocket, (struct sockaddr*)&serverDir, sizeof(serverDir)) == -1) {
+        perror("Error al conectar al servidor");
+        return -1;
+    }
+
+    printf("Conexión exitosa con el servidor %s\n", serverIP);
+
+    return mySocket;
+}
+
+// Función que maneja la conexión con el servidor en un hilo separado
+void* manejoConexion(void* args) {
+    int socket = *((int*)args);
+    free(args);
+
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("\nIngrese un comando remoto: ");
+        fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "close") == 0) {
+            break;
+        } else if (strncmp(command, "cd", 2) == 0 && strlen(command) > 3 && command[2] == ' ') {
+            // Extraer la ruta del directorio del comando
+            char directory[MAX_COMMAND_LENGTH];
+            strcpy(directory, command + 3);
+
+            // Enviar el comando de cambio de directorio al servidor
+            if (send(socket, command, strlen(command), 0) == -1) {
+                perror("Error al enviar el comando al servidor");
+                break;
+            }
+
+            // Recibir la respuesta del servidor
+            char response[MAX_RESPONSE_LENGTH];
+            ssize_t bytesRead = recv(socket, response, MAX_RESPONSE_LENGTH - 1, 0);
+            if (bytesRead == -1) {
+                perror("Error al recibir la respuesta del servidor");
+                break;
+            }
+            response[bytesRead] = '\0';
+
+            // Imprimir la respuesta del servidor
+            printf("Respuesta del servidor: %s\n", response);
+        } else if (strcmp(command, "pwd") == 0) {
+            // Enviar el comando "pwd" al servidor
+            if (send(socket, command, strlen(command), 0) == -1) {
+                perror("Error al enviar el comando al servidor");
+                break;
+            }
+
+            // Recibir la respuesta del servidor
+            char response[MAX_RESPONSE_LENGTH];
+            ssize_t bytesRead = recv(socket, response, MAX_RESPONSE_LENGTH - 1, 0);
+            if (bytesRead == -1) {
+                perror("Error al recibir la respuesta del servidor");
+                break;
+            }
+            response[bytesRead] = '\0';
+
+            // Imprimir la respuesta del servidor
+            printf("Directorio activo remoto: %s\n", response);
+        } else {
+            // Otros comandos remotos
+            // procesarComando(socket,command);
+        }
+    }
+
+   
